2c5be98376e68c2613d77d1c69460d10b0adc027
Emrul-Hasan-Emon <emrulhasanemon12@gmail.com>
2024-06-02 12:38:49 +0600

    Main: main.go is filled with data,

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/Emrul-Hasan-Emon/application/handlers"
+)
+
+const portNumber = ":8080"
+
+func main() {
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+
+	fmt.Println("Started application on port: ", portNumber)
+	_ = http.ListenAndServe(portNumber, nil)
 }
