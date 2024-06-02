f486b88c4f7d94ae0f39e2bba147bedad9703e3a
Emrul-Hasan-Emon <emrulhasanemon12@gmail.com>
2024-06-02 15:01:07 +0600

    HTML Templates: Some functionalities are added for integrating HTML layouts.

--- renderer/render.go
+++ renderer/render.go
@@ -1,16 +1,65 @@
 package renderer
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
+var functions = template.FuncMap{}
+
 func RenderTemplates(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
+	templateCache, err := CreateTemplateCache()
 	if err != nil {
-		fmt.Println("error parsing template: ", err)
-		return
+		log.Fatal(err)
 	}
+
+	myTemplate, ok := templateCache[tmpl]
+	if !ok {
+		log.Fatal(errors.New("template is missing"))
+	}
+
+	buffer := new(bytes.Buffer)
+	_ = myTemplate.Execute(buffer, nil)
+
+	_, err = buffer.WriteTo(w)
+
+	if err != nil {
+		fmt.Println("An error occured while writing template to response. The error: ", err)
+	}
+}
+
+// This function creates a template cache as a map
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+	pages, err := filepath.Glob("templates/*.page.html")
+	if err != nil {
+		return myCache, err
+	}
+
+	layouts, err := filepath.Glob("templates/*.layout.html")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		templateSet, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(layouts) != 0 {
+			templateSet, err = templateSet.ParseGlob("templates/*.layout.html")
+			if err != nil {
+				return myCache, err
+			}
+		}
+		myCache[name] = templateSet
+	}
+	return myCache, nil
 }
