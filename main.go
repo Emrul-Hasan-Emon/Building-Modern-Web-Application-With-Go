42f6ba3716a589c8c88d7e7542abf53a0c435901
Emrul-Hasan-Emon <emrulhasanemon12@gmail.com>
2024-06-02 15:20:34 +0600

    Reneder HTML Templates: renderer package is modified with AppConfig data.

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/Emrul-Hasan-Emon/application/config"
 	"github.com/Emrul-Hasan-Emon/application/handlers"
+	"github.com/Emrul-Hasan-Emon/application/renderer"
 )
 
 const portNumber = ":8080"
 
 func main() {
+	appConfig := config.CreateNewConfigInstance()
+	renderer.SetTemplateCache(appConfig.GetTemplateCache())
+
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
--- renderer/render.go
+++ renderer/render.go
@@ -7,17 +7,15 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"path/filepath"
 )
 
-var functions = template.FuncMap{}
+var templateCache map[string]*template.Template
+
+func SetTemplateCache(cache map[string]*template.Template) {
+	templateCache = cache
+}
 
 func RenderTemplates(w http.ResponseWriter, tmpl string) {
-	templateCache, err := CreateTemplateCache()
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	myTemplate, ok := templateCache[tmpl]
 	if !ok {
 		log.Fatal(errors.New("template is missing"))
@@ -26,40 +24,9 @@ func RenderTemplates(w http.ResponseWriter, tmpl string) {
 	buffer := new(bytes.Buffer)
 	_ = myTemplate.Execute(buffer, nil)
 
-	_, err = buffer.WriteTo(w)
+	_, err := buffer.WriteTo(w)
 
 	if err != nil {
 		fmt.Println("An error occured while writing template to response. The error: ", err)
 	}
-}
-
-// This function creates a template cache as a map
-func CreateTemplateCache() (map[string]*template.Template, error) {
-	myCache := map[string]*template.Template{}
-	pages, err := filepath.Glob("templates/*.page.html")
-	if err != nil {
-		return myCache, err
-	}
-
-	layouts, err := filepath.Glob("templates/*.layout.html")
-	if err != nil {
-		return myCache, err
-	}
-
-	for _, page := range pages {
-		name := filepath.Base(page)
-		templateSet, err := template.New(name).Funcs(functions).ParseFiles(page)
-		if err != nil {
-			return myCache, err
-		}
-
-		if len(layouts) != 0 {
-			templateSet, err = templateSet.ParseGlob("templates/*.layout.html")
-			if err != nil {
-				return myCache, err
-			}
-		}
-		myCache[name] = templateSet
-	}
-	return myCache, nil
 }
