b352be01aaf2d2920142348dc3e06d41ee00c196
Emrul-Hasan-Emon <emrulhasanemon12@gmail.com>
2024-06-02 12:57:05 +0600

    Handlers: Refactored renderTemplates function

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,24 +1,15 @@
 package handlers
 
 import (
-	"fmt"
-	"html/template"
 	"net/http"
+
+	"github.com/Emrul-Hasan-Emon/application/renderer"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplates(w, "home.html")
+	renderer.RenderTemplates(w, "home.html")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplates(w, "about.html")
-}
-
-func renderTemplates(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template: ", err)
-		return
-	}
+	renderer.RenderTemplates(w, "about.html")
 }
